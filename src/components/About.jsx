d1e99756049c926edb5fe44656fa50ad50f86c99
frankwan41 <80295318+frankwan41@users.noreply.github.com>
2024-03-12 15:15:14 +1100

    Finish About,Experience,Tech pages

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from "react";
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion.js";
+import { SectionWrapper } from "../hoc";
+
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <Tilt
+      className="xs:w-[250px] w-full"
+      options={{ max: 45, sclae: 1, speed: 450 }}
+    >
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+      >
+        <div className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col">
+          <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+          <h3 className="text-white text-[20px] font-bold text-center">
+            {title}
+          </h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  );
+};
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview</h2>
+      </motion.div>
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        Minim ipsum aliqua ex sunt velit. Magna et sit Lorem enim aliquip cillum
+        aliqua in et Lorem reprehenderit enim eiusmod irure. Consequat enim in
+        tempor commodo ea sunt minim ut non dolor cupidatat labore pariatur.
+      </motion.p>
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
 
-export default About
+const AboutPage = SectionWrapper(About, "about");
+
+export default AboutPage;
