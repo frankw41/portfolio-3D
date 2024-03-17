8c0e4b9d02976e9380dbdf64feeda78cef155f13
frankwan41 <80295318+frankwan41@users.noreply.github.com>
2024-03-17 18:30:27 +1100

    Change file setups

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    strictPort: true,
-    host: true,
-  },
+  base: "/portfolio-3D/",
+  // server: {
+  //   port: 3000,
+  //   strictPort: true,
+  //   host: true,
+  // },
 });
